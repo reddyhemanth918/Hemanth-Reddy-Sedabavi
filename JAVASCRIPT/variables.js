f24febf0dfec7bb8950c7c32683b3466161fd589
Hemanth <reddyhemanth918@gmail.com>
2023-04-10 21:49:23 +0530

    Day5

---
+++ JAVASCRIPT/variables.js
@@ -0,0 +1,11 @@
+a=20
+
+function sample()
+{
+    console.log(a)
+    var a=30
+    console.log(a)
+}
+
+sample()
+console.log(a)
---
+++ JAVASCRIPT/variables2.js
@@ -0,0 +1,30 @@
+/*
+ function sample(){
+    //var a=30
+    let a =30
+    console.log(a)
+    {
+        // var
+        let a =45
+        console.log(a)
+    }
+    console.log(a)
+ }
+ sample()
+*/
+
+/*
+for(var a=0;a<10;a++)  
+   console.log(a);
+
+   console.log("----------------------------------------------------------")
+
+console.log(a)  */
+
+const a=90
+for (let a=0;a<10;a++)
+     console.log(a);
+     
+     console.log("---------------")
+     a=900
+console.log(a)
