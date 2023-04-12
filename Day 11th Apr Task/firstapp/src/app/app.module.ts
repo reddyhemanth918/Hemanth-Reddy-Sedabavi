f841851828c5ad9d9bf40ed172c3b967757d97dc
hemanth <reddyhemanth918@gmail.com>
2023-04-12 16:08:28 +0530

    Assignment

---
+++ Day 11th Apr Task/firstapp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesboxComponent } from './servicesbox/servicesbox.component';
+import { SlideviewComponent } from './slideview/slideview.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ServicesboxComponent,
+    SlideviewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
