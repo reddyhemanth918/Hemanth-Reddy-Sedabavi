8930d2f176bbc31c9741fc9d1d6a6012a7154daf
hemanth <reddyhemanth918@gmail.com>
2023-04-25 16:14:04 +0530

    commit

--- Day-20th-Apr-Task/src/app/pipes/filterprice.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterpricePipe } from './filterprice.pipe';
-
-describe('FilterpricePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterpricePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- Day-20th-Apr-Task/src/app/pipes/filtertype.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltertypePipe } from './filtertype.pipe';
-
-describe('FiltertypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltertypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
